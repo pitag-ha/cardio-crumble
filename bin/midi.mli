5bb33f70937c3852d9f980ecfda9b128ff03118b
Aryan Godara <65490434+AryanGodara@users.noreply.github.com>
2023-03-21 07:41:15 -0700

    Add two new scales (#7)
    * initial commit, add minor scale
    
    Signed-off-by: aryan <aryangodara03@gmail.com>
    
    * update .ocamlformat to allow dune build @fmt, added pentatonic scale.
    
    Signed-off-by: aryan <aryangodara03@gmail.com>
    
    * fix dune build @fmt
    
    Signed-off-by: aryan <aryangodara03@gmail.com>
    
    * remove vscode from gitignore, fix ocamlformat version
    
    Signed-off-by: aryan <aryangodara03@gmail.com>
    
    * add ocamlformat and portmidi
    
    Signed-off-by: aryan <aryangodara03@gmail.com>
    
    * rem two deps
    
    Signed-off-by: aryan <aryangodara03@gmail.com>
    
    ---------
    
    Signed-off-by: aryan <aryangodara03@gmail.com>

--- bin/midi.mli
+++ bin/midi.mli
@@ -2,23 +2,22 @@ module Event = Portmidi.Portmidi_event
 
 val error_to_string : Portmidi.Portmidi_error.t -> string
 
-module Device :
-  sig
-    type t
-    val create : int -> t
-    val shutdown : t -> (unit, Portmidi.Portmidi_error.t) result
-  end
+module Device : sig
+  type t
 
-val message_on :
-  note:char -> timestamp:int32 -> ?volume:char -> unit -> Event.t
+  val create : int -> t
+  val shutdown : t -> (unit, Portmidi.Portmidi_error.t) result
+end
+
+val message_on : note:char -> timestamp:int32 -> ?volume:char -> unit -> Event.t
 
 val message_off :
   note:char -> timestamp:int32 -> ?volume:char -> unit -> Event.t
 
 val write_output : Device.t -> Portmidi.Portmidi_event.t list -> unit
 
-module Scale :
-  sig
-    type t = Major | Nice | Blue | Overtones
-    val get : base_note:int -> t -> int -> char
-  end
+module Scale : sig
+  type t = Major | Minor | Pentatonic | Nice | Blue | Overtones
+
+  val get : base_note:int -> t -> int -> char
+end
